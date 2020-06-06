6991b1fc91cb0bd745600ca06c968851c6bf045d
Rodrigo Peña <lrpena@miuandes.cl>
2020-06-06 16:40:54 -0400

    feat(login): implement basic club penguin login windows

---
+++ Calendar/Views/LoginView.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Calendar.Views
+{
+    /// <summary>
+    /// Interaction logic for LoginView.xaml
+    /// </summary>
+    public partial class LoginView : Window
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+        }
+
+        private void DoLogin(object sender, MouseButtonEventArgs e)
+        {
+            var calendarWindow = new DisplayCalendarView();
+            calendarWindow.Show();
+            this.Close();
+        }
+    }
+}
